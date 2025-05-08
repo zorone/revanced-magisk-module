ea015d222a0de77c6d61a108edf379372b6e8cd7
Imron Jehleh <imron.jehleh@gmail.com>
2025-05-09 04:58:24 +0700

    Let me try on terminal first, then.

--- utils.sh
+++ utils.sh
@@ -339,7 +339,7 @@ dl_apkmirror() {
 			tmp=$($HTMLQ "div.table-row.headerFont:nth-last-child($i)" -r "span:nth-child(n+3)" <<<"$resp")
 			arr["$i"]=$tmp
 			echo >&2 "$i: len = ${#tmp}"
-			if [ -n "$(sed -n "$arch" <<<"$tmp")" ] && [ -n "$(sed -n "$dpi" <<<"$tmp")" ]; then
+			if [ -n "$(sed -n '$arch' <<<"$tmp")" ] && [ -n "$(sed -n '$dpi' <<<"$tmp")" ]; then
 				echo "${arr[$i]}";
 			fi
 			i=`expr $i + 1`;
