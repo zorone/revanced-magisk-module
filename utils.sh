04385999f5d2414ddd1190040e3eef138f633e0f
Imron Jehleh <imron.jehleh@gmail.com>
2025-05-09 04:52:04 +0700

    I am so tired!

--- utils.sh
+++ utils.sh
@@ -339,7 +339,7 @@ dl_apkmirror() {
 			tmp=$($HTMLQ "div.table-row.headerFont:nth-last-child($i)" -r "span:nth-child(n+3)" <<<"$resp")
 			arr["$i"]=$tmp
 			echo >&2 "$i: len = ${#tmp}"
-			if [ $(grep -irn "$arch" <<<"$tmp") ] && [ $(grep -irn "$dpi" <<<"$tmp") ]; then
+			if [ $(sed -n "$arch" <<<"$tmp") ] && [ $(sed -n "$dpi" <<<"$tmp") ]; then
 				echo "${arr[$i]}";
 			fi
 			i=`expr $i + 1`;
