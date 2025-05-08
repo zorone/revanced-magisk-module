19270ad759a84d25787b1c29ef1df6771ca34b3d
Imron Jehleh <imron.jehleh@gmail.com>
2025-05-09 04:45:52 +0700

    No! I didn't want to find like that. Why the heck it tried to find from file...

--- utils.sh
+++ utils.sh
@@ -339,7 +339,7 @@ dl_apkmirror() {
 			tmp=$($HTMLQ "div.table-row.headerFont:nth-last-child($i)" -r "span:nth-child(n+3)" <<<"$resp")
 			arr["$i"]=$tmp
 			echo >&2 "$i: len = ${#tmp}"
-			if [ $(grep -iq "$arch" <<<"$tmp") ] && [ $(grep -iq "$dpi" <<<"$tmp") ]; then
+			if [ $(grep -irn "$arch" <<<"$tmp") ] && [ $(grep -irn "$dpi" <<<"$tmp") ]; then
 				echo "${arr[$i]}";
 			fi
 			i=`expr $i + 1`;
