13e68988d0c61ea0abafc584291965eb8852782b
Imron Jehleh <imron.jehleh@gmail.com>
2025-05-09 16:00:08 +0700

    Nope.

--- build.sh
+++ build.sh
@@ -144,8 +144,8 @@ for table_name in $(toml_get_table_names); do
 	if [ "${app_args[arch]}" = all ]; then
 		declare res=0
 		idx=$((idx + 1))
-		(build_rv "$(declare -p app_args)" &) | read -r ret
-		wait -n
+		build_rv "$(declare -p app_args)" | read -r ret &
+		wait -n	
 		echo "Get return code: $ret"
 	fi
 	if [ "${app_args[arch]}" = both ]; then
